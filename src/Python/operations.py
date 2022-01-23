c6ab8134235bec9ba8a6ce43ac07975e8dcff331
rohanrudra55 <rohanrudra55@gmail.com>
2022-01-23 14:31:26 +0530

    Restructured

--- src/Python/operations.py
+++ src/Python/operations.py
@@ -1,11 +1,30 @@
-# #It contains the differnt test functions
+"""MIT License
+
+Copyright (c) 2022 Rohan Rudra
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+# It contains the differnt test functions
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-try:
-    import parameters as pmt
-except:
-    print('Error in module !')
+import parameters as pmt
 
 def output(image ,caption = "Image" ,save = False ,show = False):
     # To display the image and also to save the result
--- src/Python/to3d.py
+++ src/Python/to3d.py
@@ -1,12 +1,29 @@
+"""MIT License
+
+Copyright (c) 2022 Rohan Rudra
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 #Importing the required libraries
 import numpy as np
 import cv2
-try:
-    import open3d
-
-except ImportError:
-    print("Please Install Open3D")
-    exit()
+import open3d
 
 
 # Converts 2D matrix to 3D point cloud
@@ -16,28 +33,18 @@ def convert_to_3d(left_image_matrix, depth_map_matrix):   # Depth map should be 
     left_image_matrix = cv2.resize(left_image_matrix,  depth_map_matrix.shape[::-1])
     left_image_matrix = cv2.cvtColor(left_image_matrix, cv2.COLOR_BGR2RGB)
 
-    height, width, _ = left_image_matrix.shape
+    height, width, depth = left_image_matrix.shape
 
-    point_color = [] 
+    point_color = np.array(left_image_matrix.reshape((height*width), depth)) 
+    z_coordinates = depth_map_matrix.flatten()
+    points = [] 
+    index=0
     # Stores Color Information of the original Image
     for row_pixel in range(height):
         for coloum_pixel in range(width):
-            point_color.append(left_image_matrix[row_pixel][coloum_pixel])
-    point_color = np.array(point_color)
-
-    y_coordinates = []
-    x_coordinates = []
-
-    for row_pixel in range(height):
-        for column_pixel in range(width):
-            y_coordinates.append(column_pixel)
-            x_coordinates.append(row_pixel)
- 
-    z_coordinates = depth_map_matrix.flatten()
-    points = [] 
-
-    for index in range(len(x_coordinates)):
-        points.append([x_coordinates[index], y_coordinates[index], z_coordinates[index]])
+            points.append([row_pixel, coloum_pixel, z_coordinates[index]])
+            index+=1
+            
 
     # Visualize the point Cloud
     point_cloud = open3d.geometry.PointCloud()
